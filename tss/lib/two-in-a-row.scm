1914e34cb6cfcf028a8d56f54000a4055bd1c2f9
John Trammell <johntrammell@gmail.com>
2010-01-11 10:18:21 -0600

    completed the exercise

--- tss/lib/two-in-a-row.scm
+++ tss/lib/two-in-a-row.scm
@@ -1,8 +1,28 @@
 
-(define two-in-a-row?
-  (lambda (a lat)
+;;; (define is-first? (a lat)
+;;;   (lambda (a lat)
+;;;     (cond
+;;;       ((null? lat) #f)
+;;;       (else (eq? a (car lat))))))
+
+;;; (define two-in-a-row?
+;;;   (lambda (lat)
+;;;     (cond
+;;;       ((null? lat) #f)
+;;;       (else
+;;;         (or (is-first? (car lat) (cdr lat))
+;;;             (two-in-a-row? (cdr lat)))))))
+
+(define two-in-a-row-b?
+  (lambda (preceding lat)
     (cond
       ((null? lat) #f)
-      (else (or (eq? a (car lat))
-                (member? a (cdr lat)))))))
+      (else (or (eq? preceding (car lat))
+                (two-in-a-row-b? (car lat) (cdr lat)))))))
+
+(define two-in-a-row?
+  (lambda (lat)
+    (cond
+      ((null? lat) #f)
+      (else (two-in-a-row-b? (car lat) (cdr lat))))))
 
